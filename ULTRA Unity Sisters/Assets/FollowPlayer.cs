f001f36c253991197d38798844434ab4d813d282
nobarudo <s13t203@stmail.eng.kagawa-u.ac.jp>
2015-12-19 07:09:57 +0900

    カメラの挙動の変更

--- ULTRA Unity Sisters/Assets/FollowPlayer.cs
+++ ULTRA Unity Sisters/Assets/FollowPlayer.cs
@@ -4,14 +4,17 @@ using System.Collections;
 public class FollowPlayer : MonoBehaviour {
     public Transform target;
     private Vector3 offset;
+    private Vector3 camePos;
 
-	// Use this for initialization
-	void Start () {
-        offset = GetComponent<Transform>().position - target.position;
-	}
+    // Use this for initialization
+    void Start () {
+        offset = GetComponent<Transform>().position;
+        camePos = offset;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        GetComponent<Transform>().position = target.position + offset;
+        camePos.x = target.position.x + offset.x;
+        GetComponent<Transform>().position = camePos;
 	}
 }
